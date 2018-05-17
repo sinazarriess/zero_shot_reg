2e82dcffcffecf31d05d854283ee7d410c0339f2
liliSchroeder <lschroed@techfak.uni-bielefeld.de>
2018-05-17 11:34:16 +0200

    some clean-up. new basic code for evaluation added

---
+++ src/bleu.py
@@ -0,0 +1,73 @@
+import cPickle as pickle
+import os
+import sys
+import json
+sys.path.append('../coco-caption')
+
+from pycocoevalcap.bleu.bleu import Bleu
+from pycocoevalcap.rouge.rouge import Rouge
+from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.meteor.meteor import Meteor
+
+
+def score(ref, hypo):
+    scorers = [
+        (Bleu(4),["Bleu_1","Bleu_2","Bleu_3","Bleu_4"]),
+        (Meteor(),"METEOR"),
+        (Rouge(),"ROUGE_L"),
+        (Cider(),"CIDEr")
+    ]
+    final_scores = {}
+    for scorer,method in scorers:
+        score,scores = scorer.compute_score(ref,hypo)
+        if type(score)==list:
+            for m,s in zip(method,score):
+                final_scores[m] = s
+        else:
+            final_scores[method] = score
+
+    return final_scores
+    
+
+def evaluate(reference_path, candidate_path, split='val'):
+    
+    # load caption data
+    with open(reference_path, 'rb') as f:
+        ref = json.load(f)
+    with open(candidate_path, 'rb') as f:
+        cand = json.load(f)
+    
+    print "ref keys",len(ref),ref.keys()[:10]    
+    print "cand keys",len(cand),cand.keys()[:10]    
+    print set(ref.keys())-set(cand.keys())
+    sortkeys = sorted(ref.keys())
+    refsort = {k:ref[k] for k in sortkeys}
+    candsort = {k:cand[k][:1] for k in sortkeys}
+    # compute bleu score
+    final_scores = score(refsort, candsort)
+
+    # print out scores
+    print 'Bleu_1:\t',final_scores['Bleu_1']  
+    print 'Bleu_2:\t',final_scores['Bleu_2']  
+    print 'Bleu_3:\t',final_scores['Bleu_3']  
+    print 'Bleu_4:\t',final_scores['Bleu_4']  
+    print 'METEOR:\t',final_scores['METEOR']  
+    print 'ROUGE_L:',final_scores['ROUGE_L']  
+    print 'CIDEr:\t',final_scores['CIDEr']
+    
+    
+    return final_scores
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
---
+++ src/run_jupyter_fixedport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# based on https://help.rc.ufl.edu/doc/Remote_Jupyter_Notebook
+#SBATCH --job-name=jupyter
+#SBATCH --output=jupyter_%j.log
+#SBATCH --mail-user=lschroed@techfak.uni-bielefeld.de
+#SBATCH --mail-type=END
+date;hostname;pwd
+export PATH=/media/compute/vol/dsg/lilian/anaconda2/bin${PATH:+:${PATH}}
+export PATH=/media/compute/vol/cuda/9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/media/compute/vol/cuda/9.0/lib64${LD_LIBRARY_PATH:+:${LD
+_L
+IBRARY_PATH}}
+export CUDA_HOME=/media/compute/vol/cuda/9.0
+# activate conda env with tensorflow:
+source /media/compute/vol/dsg/lilian/anaconda2/bin/activate tf-lilian
+port=21153
+echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
+echo -e "\nSSH tunnel command: ssh -J ${USER}@shell.techfak.de,${USER}@login.gpu.cit-ec.net -L 8080:$(hostname):${port} ${USER}@login.gpu.cit-ec.net"
+echo -e "\nLocal URI: http://localhost:8080"
+echo -e "\nYou need to copy the token from the logfile of this job."
+# the following seems to be necessary to avoid permission
+# problems with trying to write to /run :
+export XDG_RUNTIME_DIR=""
+jupyter notebook --no-browser --port=${port} --ip='*'
+date
