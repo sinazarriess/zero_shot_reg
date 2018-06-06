0fc2d1e10febb02394b9be96b6bf0c3df20c74a6
liliSchroeder <lschroed@techfak.uni-bielefeld.de>
2018-06-06 16:44:54 +0200

    list of categories; added refexp ids to analyse output

--- src/eval/analyse_unknown_prediction.py
+++ src/eval/analyse_unknown_prediction.py
@@ -12,6 +12,8 @@ with open('./model/without_unknown/inject_refcoco_refrnn_compositional_3_512_1/4
 with open('./model/inject_refcoco_refrnn_compositional_3_512_1/4evalinject_refcoco_refrnn_compositional_3_512_1.json') as f:
     unknown_candidate = json.load(f)
 
+refcoco_data = pd.read_json("./../../data/refcoco_refdf.json.gz", orient="split", compression="gzip")
+bounding_boxes = pd.read_json('../../data/mscoco_bbdf.json.gz', orient="split", compression="gzip")
 
 unknown_counter = 0
 print "Number of images in test set: ", len(reference.keys())
@@ -29,20 +31,27 @@ for refex_id in unknown_candidate:
 
 print "Number of unknown occurences: ", unknown_counter
 
-with open('./jsons/unknown_analysis.json', 'w') as f:
-    json.dump(analysis_dict, f)
-
-
 images = set()
-refcoco_data = pd.read_json("./../../data/refcoco_refdf.json.gz", orient="split", compression="gzip")
-
 regionids = list()
+
 for id in analysis_dict.keys():
     regionids.append(str(id))
-print regionids
 
 for index, row in refcoco_data.iterrows():
     if str(row['region_id']) in regionids:
         images.add(row['image_id'])
+        analysis_dict[str(row['region_id'])]['image_id'] = row['image_id']
+        analysis_dict[str(row['region_id'])]['refexp_id'] = row['rex_id']
+
+for index, row in bounding_boxes.iterrows():
+    if str(row['region_id']) in regionids:
+        analysis_dict[str(row['region_id'])]['cat'] = row['cat']
+        analysis_dict[str(row['region_id'])]['bb'] = row['bb']
+
+print analysis_dict[analysis_dict.keys()[0]]
 
 print len(images)
+print images
+
+with open('./jsons/unknown_analysis.json', 'w') as f:
+    json.dump(analysis_dict, f)
