9a174b260df7316862753d08a3aa1d081a6c0b42
liliSchroeder <lschroed@techfak.uni-bielefeld.de>
2018-06-14 17:09:35 +0200

    added file

--- src/eval/generatecaptionsfromstoredmodel.py
+++ src/eval/generatecaptionsfromstoredmodel.py
@@ -6,23 +6,27 @@ import numpy as np
 import lstm.params as p
 from collections import defaultdict
 
+
+model_dir = './model/with_unknown/'
+edge_index = 0
+
 def final_prediction(self, tensor):
     return tensor[:, -1]
 
 if __name__ == "__main__":
 
-    filenames = np.genfromtxt('../eval/cached_data/raw_dataset_filenames.txt', delimiter=',', dtype=None, encoding=None)
+    filenames = np.genfromtxt(model_dir + 'raw_dataset_filenames.txt', delimiter=',', dtype=None, encoding=None)
 
-    with open('../eval/cached_data/index2token.json', "r") as f:
+    with open(model_dir + 'index2token.json', "r") as f:
         index2token = json.load(f)
     new_index2token= {int(key): value for key, value in index2token.iteritems()}
 
     # keep unknown tokens
     new_index2token[1] = "UNKNOWN"
 
-    images = np.genfromtxt('../eval/cached_data/raw_dataset.txt', delimiter=',', dtype=None)
+    images = np.genfromtxt(model_dir + 'raw_dataset.txt', delimiter=',', dtype=None)
 
-    imported_meta = tf.train.import_meta_graph('../eval/model/without_chicken/inject_refcoco_refrnn_compositional_3_512_1/model.meta')
+    imported_meta = tf.train.import_meta_graph(model_dir + 'inject_refcoco_refrnn_compositional_3_512_1/model.meta')
     graph = tf.get_default_graph()
     seq_in = graph.get_tensor_by_name('seq_in:0')
     seq_len = graph.get_tensor_by_name('seq_len:0')
@@ -32,18 +36,36 @@ if __name__ == "__main__":
 
     with tf.Session() as sess:
 
-        imported_meta.restore(sess, tf.train.latest_checkpoint('../eval/model/without_chicken/inject_refcoco_refrnn_compositional_3_512_1'))
+        imported_meta.restore(sess, tf.train.latest_checkpoint(model_dir + 'inject_refcoco_refrnn_compositional_3_512_1'))
 
         oids = list()
         captions = list()
-        searcher = lstm.beam.Search(new_index2token, True)
+        captions_greedy = list()
+      #  searcher = lstm.beam.Search(new_index2token, True)
+      #  for (i, image_input) in enumerate(images):
+      #      caption = searcher.generate_sequence_beamsearch(lambda prefixes: sess.run(last_prediction, feed_dict={
+      #          seq_in: prefixes,
+      #          seq_len: [len(p) for p in prefixes],
+      #          image: image_input.reshape([1, -1]).repeat(len(prefixes), axis=0)
+      #      }))
+      #      captions.append(caption)
+
         for (i, image_input) in enumerate(images):
-            caption = searcher.generate_sequence_beamsearch(lambda prefixes: sess.run(last_prediction, feed_dict={
-                seq_in: prefixes,
-                seq_len: [len(p) for p in prefixes],
-                image: image_input.reshape([1, -1]).repeat(len(prefixes), axis=0)
+            predictions_function = (lambda prefix: sess.run(last_prediction, feed_dict={
+                seq_in: [prefix],
+                seq_len: [len(prefix)],
+                image: image_input.reshape([1, -1]).repeat(1, axis=0)
             }))
-            captions.append(caption)
+            gen_prefix = list()
+            isComplete = False
+            #while not isComplete:
+            indexes_distributions = predictions_function(gen_prefix)
+
+         #   for (next_index, next_prob) in enumerate(indexes_distribution):
+            #if next_word == edge_index:
+            #    isComplete = True
+            print gen_prefix
+
         for (i, item) in enumerate(filenames):
             oids.append(str(item).split("_")[1])
 
@@ -51,6 +73,6 @@ if __name__ == "__main__":
         for (idx, pair) in enumerate(zip(oids, captions)):
             dict4eval[pair[0]] = [pair[1]]
 
-        with open('../eval/jsons/restoredmodel_captions_chicken.json', 'w') as f:
+        with open(model_dir + 'restoredmodel_captions_with_unknown.json', 'w') as f:
             json.dump(dict4eval, f)
 
